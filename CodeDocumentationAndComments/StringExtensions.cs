728b930104c9a3b35c4d640921409dce86813a21
Zlatimir Mihaylov <zlatimirmihailov@gmail.com>
2015-06-27 18:42:17 +0300

    Documentation and comments hw

---
+++ CodeDocumentationAndComments/StringExtensions.cs
@@ -0,0 +1,292 @@
+﻿namespace Telerik.ILS.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    public static class StringExtensions
+    {
+        public static string ToMd5Hash(this string input)
+        {
+            var md5Hash = MD5.Create();
+
+            // Convert the input string to a byte array and compute the hash.
+            var data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            // Create a new StringBuilder to collect the bytes
+            // and create a string.
+            var builder = new StringBuilder();
+
+            // Loop through each byte of the hashed data 
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                builder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// Compares if the given string analogue to the boolean variable true
+        /// </summary>
+        /// <param name="input">Accepts a string variable</param>
+        /// <returns>returns a boolean variable</returns>
+        public static bool ToBoolean(this string input)
+        {
+            var stringTrueValues = new[] { "true", "ok", "yes", "1", "да" };
+            return stringTrueValues.Contains(input.ToLower());
+        }
+
+        /// <summary>
+        /// Parses to Int16 number
+        /// </summary>
+        /// <param name="input">Accepts a string representation of a 32bit integer number</param>
+        /// <returns>Return a 16bit integer number</returns>
+        public static short ToShort(this string input)
+        {
+            short shortValue;
+            short.TryParse(input, out shortValue);
+            return shortValue;
+        }
+
+        /// <summary>
+        /// Parses to Int32 number
+        /// </summary>
+        /// <param name="input">Accepts a string representation of a 32bit integer number</param>
+        /// <returns>Return a 32bit integer number</returns>
+        public static int ToInteger(this string input)
+        {
+            int integerValue;
+            int.TryParse(input, out integerValue);
+            return integerValue;
+        }
+
+        /// <summary>
+        /// Parses to Int64 number
+        /// </summary>
+        /// <param name="input">Accepts a string representation of a 64bit integer number</param>
+        /// <returns>Return a 64bit integer number</returns>
+        public static long ToLong(this string input)
+        {
+            long longValue;
+            long.TryParse(input, out longValue);
+            return longValue;
+        }
+
+        /// <summary>
+        /// Parses to DateTime variable
+        /// </summary>
+        /// <param name="input">Accepts a string in the correct datetime format</param>
+        /// <returns>Returns a datetime variable</returns>
+        public static DateTime ToDateTime(this string input)
+        {
+            DateTime dateTimeValue;
+            DateTime.TryParse(input, out dateTimeValue);
+            return dateTimeValue;
+        }
+
+        /// <summary>
+        /// Makes the first letter of a string Capital
+        /// </summary>
+        /// <param name="input">Accepts a string</param>
+        /// <returns>Returns a string</returns>
+        public static string CapitalizeFirstLetter(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            return input.Substring(0, 1).ToUpper(CultureInfo.CurrentCulture) + input.Substring(1, input.Length - 1);
+        }
+
+        /// <summary>
+        /// Gets a string between two others
+        /// </summary>
+        /// <param name="input">Accepts a string</param>
+        /// <param name="startString">Start string</param>
+        /// <param name="endString">End string</param>
+        /// <param name="startFrom">Indexx to start from</param>
+        /// <returns></returns>
+        public static string GetStringBetween(this string input, string startString, string endString, int startFrom = 0)
+        {
+            input = input.Substring(startFrom);
+            startFrom = 0;
+            if (!input.Contains(startString) || !input.Contains(endString))
+            {
+                return string.Empty;
+            }
+
+            var startPosition = input.IndexOf(startString, startFrom, StringComparison.Ordinal) + startString.Length;
+            if (startPosition == -1)
+            {
+                return string.Empty;
+            }
+
+            var endPosition = input.IndexOf(endString, startPosition, StringComparison.Ordinal);
+            if (endPosition == -1)
+            {
+                return string.Empty;
+            }
+
+            return input.Substring(startPosition, endPosition - startPosition);
+        }
+
+        /// <summary>
+        /// Convert cyrillic to latin letters
+        /// </summary>
+        /// <param name="input">Accepts a string with cyrilic letters</param>
+        /// <returns>Returns a string with the corresponding latin letters</returns>
+        public static string ConvertCyrillicToLatinLetters(this string input)
+        {
+            var bulgarianLetters = new[]
+                                       {
+                                           "а", "б", "в", "г", "д", "е", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п",
+                                           "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ь", "ю", "я"
+                                       };
+            var latinRepresentationsOfBulgarianLetters = new[]
+                                                             {
+                                                                 "a", "b", "v", "g", "d", "e", "j", "z", "i", "y", "k",
+                                                                 "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "h",
+                                                                 "c", "ch", "sh", "sht", "u", "i", "yu", "ya"
+                                                             };
+            for (var i = 0; i < bulgarianLetters.Length; i++)
+            {
+                input = input.Replace(bulgarianLetters[i], latinRepresentationsOfBulgarianLetters[i]);
+                input = input.Replace(bulgarianLetters[i].ToUpper(), latinRepresentationsOfBulgarianLetters[i].CapitalizeFirstLetter());
+            }
+
+            return input;
+        }
+
+        /// <summary>
+        /// Converts latin to cirylic letters
+        /// </summary>
+        /// <param name="input">Accepts a string with latin letters</param>
+        /// <returns>Returns a string with the corresponding cyrilic letters</returns>
+        public static string ConvertLatinToCyrillicKeyboard(this string input)
+        {
+            var latinLetters = new[]
+                                   {
+                                       "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+                                       "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+                                   };
+
+            var bulgarianRepresentationOfLatinKeyboard = new[]
+                                                             {
+                                                                 "а", "б", "ц", "д", "е", "ф", "г", "х", "и", "й", "к",
+                                                                 "л", "м", "н", "о", "п", "я", "р", "с", "т", "у", "ж",
+                                                                 "в", "ь", "ъ", "з"
+                                                             };
+
+            for (int i = 0; i < latinLetters.Length; i++)
+            {
+                input = input.Replace(latinLetters[i], bulgarianRepresentationOfLatinKeyboard[i]);
+                input = input.Replace(latinLetters[i].ToUpper(), bulgarianRepresentationOfLatinKeyboard[i].ToUpper());
+            }
+
+            return input;
+        }
+
+        /// <summary>
+        /// Converst to latin if needed, and replaces all not significant symbols
+        /// </summary>
+        /// <param name="input">Accepts a string</param>
+        /// <returns>Returns a valid string for username</returns>
+        public static string ToValidUsername(this string input)
+        {
+            input = input.ConvertCyrillicToLatinLetters();
+            return Regex.Replace(input, @"[^a-zA-z0-9_\.]+", string.Empty);
+        }
+
+        /// <summary>
+        /// Makes incorrect filenames correct!
+        /// </summary>
+        /// <param name="input">Accepts a string</param>
+        /// <returns>Returns a valid latin file name</returns>
+        public static string ToValidLatinFileName(this string input)
+        {
+            input = input.Replace(" ", "-").ConvertCyrillicToLatinLetters();
+            return Regex.Replace(input, @"[^a-zA-z0-9_\.\-]+", string.Empty);
+        }
+
+        /// <summary>
+        /// Gets first characters from a string by given count!
+        /// </summary>
+        /// <param name="input">Accepts a string</param>
+        /// <param name="charsCount">Count of characters to return</param>
+        /// <returns>Returns a new string with the specific length</returns>
+        public static string GetFirstCharacters(this string input, int charsCount)
+        {
+            return input.Substring(0, Math.Min(input.Length, charsCount));
+        }
+
+        /// <summary>
+        /// Gets the extension of a given file
+        /// </summary>
+        /// <param name="fileName">Accepts a string</param>
+        /// <returns>Returns the extension as a new string</returns>
+        public static string GetFileExtension(this string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                return string.Empty;
+            }
+
+            string[] fileParts = fileName.Split(new[] { "." }, StringSplitOptions.None);
+            if (fileParts.Count() == 1 || string.IsNullOrEmpty(fileParts.Last()))
+            {
+                return string.Empty;
+            }
+
+            return fileParts.Last().Trim().ToLower();
+        }
+
+        /// <summary>
+        /// Gets the type of the file extension
+        /// </summary>
+        /// <param name="fileExtension">Accepts a string</param>
+        /// <returns>Returns the type of the extension as string</returns>
+        public static string ToContentType(this string fileExtension)
+        {
+            var fileExtensionToContentType = new Dictionary<string, string>
+                                                 {
+                                                     { "jpg", "image/jpeg" },
+                                                     { "jpeg", "image/jpeg" },
+                                                     { "png", "image/x-png" },
+                                                     {
+                                                         "docx",
+                                                         "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+                                                     },
+                                                     { "doc", "application/msword" },
+                                                     { "pdf", "application/pdf" },
+                                                     { "txt", "text/plain" },
+                                                     { "rtf", "application/rtf" }
+                                                 };
+            if (fileExtensionToContentType.ContainsKey(fileExtension.Trim()))
+            {
+                return fileExtensionToContentType[fileExtension.Trim()];
+            }
+
+            return "application/octet-stream";
+        }
+
+        /// <summary>
+        /// Gets the binary representation if a given string
+        /// </summary>
+        /// <param name="input">Accepts a string</param>
+        /// <returns>Returns an array of bytes</returns>
+        public static byte[] ToByteArray(this string input)
+        {
+            var bytesArray = new byte[input.Length * sizeof(char)];
+            Buffer.BlockCopy(input.ToCharArray(), 0, bytesArray, 0, bytesArray.Length);
+            return bytesArray;
+        }
+    }
+}
