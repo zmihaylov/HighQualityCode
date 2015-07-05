49d22f54033bdea458f878163c744a90810188eb
Zlatimir Mihaylov <zlatimirmihailov@gmail.com>
2015-07-05 22:37:10 +0300

    variables

---
+++ VariablesDataExpressionsConstants/PrintStatistics/PrintStatisticsTask.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintStatistics
+{
+    class PrintStatisticsTask
+    {
+        static void Main(string[] args)
+        {
+            double[] testArray = new double[5] {2.123,123.123,12313,43,34};
+            double max = FindMaxInArray(testArray);
+            double min = FindMinInArray(testArray);
+            double average = FindAverageInArray(testArray);
+            PrintInformation(min, max, average);
+        }
+
+        public static double FindMaxInArray(double[] array)
+        {
+            double max = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+            }
+
+            return max;
+        }
+
+        private static double FindMinInArray(double[] array)
+        {
+            double min = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+            }
+
+            return min;
+        }
+
+        private static double FindAverageInArray(double[] array)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            return sum / array.Length;
+        }
+
+        private static void PrintInformation(double minElement, double maxElement, double averageOfElements)
+        {
+            Console.WriteLine("Max element: {0}; Min element: {1}; Average: {2}", maxElement, minElement, averageOfElements);
+        }
+    }
+}
---
+++ VariablesDataExpressionsConstants/Size/Sizes.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Size
+{
+    class Sizes
+    {
+        public class Size
+        {
+            private double width;
+            private double height;
+
+            public Size(double width, double height)
+            {
+                this.Width = width;
+                this.Height = height;
+            }
+
+            public double Width
+            {
+                get
+                {
+                    return this.width;
+                }
+                set
+                {
+                    if (value < 0)
+                    {
+                        throw new ArgumentException();
+                    }
+                    this.width = value;
+                }
+            }
+
+            public double Height 
+            {
+                get
+                {
+                    return this.height;
+                }
+                set
+                {
+                    if (value < 0)
+                    {
+                        throw new ArgumentException();
+                    }
+                    this.height = value;
+                }
+            }
+
+            public static Size GetRotatedSize(Size size, double figureRotationAngle)
+            {
+                var angleCos = Math.Abs(Math.Cos(figureRotationAngle));
+                var angleSin = Math.Abs(Math.Sin(figureRotationAngle));
+                var width = angleCos * size.Width + angleSin * size.Height;
+                var height = angleSin * size.Width + angleCos * size.Height;
+                var rotatedSize = new Size(width, height);
+
+                return rotatedSize;
+            }
+        }
+
+        static void Main()
+        {
+            Size newSize = new Size(2.4, 4.5);
+            Console.WriteLine("width: {0:F5}; height: {1:F5}", newSize.Width, newSize.Height);
+            newSize = Size.GetRotatedSize(newSize, 130);
+            Console.WriteLine("width: {0:F5}; height: {1:F5}", newSize.Width, newSize.Height);
+        }
+    }
+}
